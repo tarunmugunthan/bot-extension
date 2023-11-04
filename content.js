9d83c9234f7435e46f7aa0a203944f3b98801e48
tarunmugunthan <42838679+tarunmugunthan@users.noreply.github.com>
2023-11-04 13:30:30 -0700

    Add files via upload

---
+++ content.js
@@ -0,0 +1,32 @@
+console.log("hi")
+
+email_elements = document.getElementsByClassName("iA g6")
+
+const list = [1, 2, 3]
+
+list.forEach(l => {
+    console.log("List item: ", l)
+})
+
+console.log(email_elements[0])
+console.log("Email elements: ", email_elements);
+
+
+// let email_elements_array = Array.from(email_elements)
+// console.log("email elem array:", email_elements_array)
+// email_elements_array.forEach(element => {
+//     console.log("Email element: ", element)
+// })
+
+// for (let i of email_elements) {
+//     console.log(i);
+// }
+
+// email_elements.forEach(element => {
+//     console.log(element.innerText);
+// });
+
+
+// foreach(e : email_elements){
+//     console.log(e.innerText);
+// }    
